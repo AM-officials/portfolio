46c10ce66976b32313c1ad538d727b57b69e9c33
Anmol Mishra <anmolmishra2005a@gmail.com>
2024-06-16 12:19:00 +0530

    A new element is added

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "AM.co",
+  description: "Turning your dream website into reality",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/AM.png" sizes="any" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ data/index.ts
@@ -0,0 +1,203 @@
+export const navItems = [
+  { name: "About", link: "#abou" },
+  { name: "Projects", link: "#proj" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Your satisfaction my commitment: High-Quality Websites ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/g1.jpg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Working to build your global audience",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Stay ahead",
+    description: "always evolving my tech-stack to:-",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Gluttony for tech, -Fueling Innovation Nonstop.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently crafting an experience for anime fans",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Lets join forces on an exciting new project!",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+   {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p2.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "Post",
+    web: "https://www.linkedin.com/posts/anmol-mishra-a5614427b_webdevelopment-3ddesign-responsivedesign-activity-7207250979036045312-ly9L?utm_source=share&utm_medium=member_desktop",
+  }, 
+  {
+    id: 1,
+    title: "Interior Designer Landing Page",
+    des: "Created a minimalistic HTML CSS landing page on client's request",
+    img: "/p1.png",
+    iconLists: ["/html.png", "/tail.svg", "/js.png"],
+    link: "Live Site",
+    web: "https://mdinteriors.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "First freelancing project",
+    des: "Created an UI template for a start up Cherry Glitz",
+    img: "/p3.png",
+    iconLists: ["/html.png", "/CSS.png", "/js.png"],
+    link: "Post",
+    web: "https://www.linkedin.com/posts/anmol-mishra-a5614427b_webdevelopment-freelancer-digitalsolutions-activity-7179703901816778753-2kDi?utm_source=share&utm_medium=member_desktop",
+  },
+  /*{
+    id: 3,
+    title: "AI Image SaaS - Canva Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+    web: "",
+  },*/
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Anmol was a great decision. We had tried out two other freelancers previously who took the advance but while delivering the project didn't give a satisfying result. Anmol on the other hand created us his first freelancing project with utmost sincerity and a much more satisfying project in a matter of days. Anmol should really be your choice if you want a professional and premium experience.",
+    name: "Subham Panda",
+    title: "Co-Founder of Cherry Glitz",
+    img:"/prof1.png",
+  },
+  {
+    quote:
+      "I always wanted to have an online presence to look more professional to potential clients but didn't know whom to reach out. Luckily I took an interior designing project for Anmol and he showed me his works and i found what i've been searching for so long. If creating an online presence through a completely captivating website is what you need Anmol should be your choice without a doubt.",
+    name: "MD Meraj",
+    title: "Interior Designer",
+    img:"/prof2.png",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Indie Game Dev - Unreal 5",
+    desc: "Designed and developed 3D games using Unreal Engine 5.4.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance Web Dev Project",
+    desc: "Led the dev of a web app for a client, from initial concept to deployment on hostinger.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies for a scaleable codebase.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 2,
+    img: "/insta.svg",
+    link: "https://www.instagram.com/stagename_a.m?igsh=bzE5M2k5dnB6dGp0"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/anmol-mishra-a5614427b/"
+  },
+];
